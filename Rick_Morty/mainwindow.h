e9fbb3583133e2cb360a5308653960bd22db52ba
raul-daza <raul.daza@udea.edu.co>
2023-11-13 12:02:07 -0500

    "creacion clase obstaculo"

--- Rick_Morty/mainwindow.h
+++ Rick_Morty/mainwindow.h
@@ -10,7 +10,7 @@
 #include <QTimer>
 #include <QPixmap>
 #include <QRect>
-#include <QDesktopWidget>
+//#include <QDesktopWidget>
 #include <QDebug>
 #include <QResizeEvent>
 #include <QGraphicsView>
