5a8f05a92d4d5788dd19a960a5d771ad93daf8cc
Luis Muñoz <luisd.munoz@udea.edu.co>
2023-12-03 14:34:31 -0500

    Informe final

--- build-Rick_Morty-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ build-Rick_Morty-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[14];
-    char stringdata0[215];
+    QByteArrayData data[15];
+    char stringdata0[230];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -42,18 +42,19 @@ QT_MOC_LITERAL(6, 70, 21), // "enemyBulletGeneration"
 QT_MOC_LITERAL(7, 92, 9), // "colission"
 QT_MOC_LITERAL(8, 102, 12), // "cambioEscena"
 QT_MOC_LITERAL(9, 115, 19), // "movimientoPersonaje"
-QT_MOC_LITERAL(10, 135, 27), // "on_progressBar_valueChanged"
-QT_MOC_LITERAL(11, 163, 5), // "value"
-QT_MOC_LITERAL(12, 169, 38), // "on_progressBar_Puntuacion_val..."
-QT_MOC_LITERAL(13, 208, 6) // "update"
+QT_MOC_LITERAL(10, 135, 14), // "aumentaPuntaje"
+QT_MOC_LITERAL(11, 150, 27), // "on_progressBar_valueChanged"
+QT_MOC_LITERAL(12, 178, 5), // "value"
+QT_MOC_LITERAL(13, 184, 38), // "on_progressBar_Puntuacion_val..."
+QT_MOC_LITERAL(14, 223, 6) // "update"
 
     },
     "MainWindow\0obstacleMove\0\0generateObstacles\0"
     "bulletMove\0enemyGeneration\0"
     "enemyBulletGeneration\0colission\0"
     "cambioEscena\0movimientoPersonaje\0"
-    "on_progressBar_valueChanged\0value\0"
-    "on_progressBar_Puntuacion_valueChanged\0"
+    "aumentaPuntaje\0on_progressBar_valueChanged\0"
+    "value\0on_progressBar_Puntuacion_valueChanged\0"
     "update"
 };
 #undef QT_MOC_LITERAL
@@ -64,7 +65,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      11,   14, // methods
+      12,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -72,17 +73,18 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   69,    2, 0x08 /* Private */,
-       3,    0,   70,    2, 0x08 /* Private */,
-       4,    0,   71,    2, 0x08 /* Private */,
-       5,    0,   72,    2, 0x08 /* Private */,
-       6,    0,   73,    2, 0x08 /* Private */,
-       7,    0,   74,    2, 0x08 /* Private */,
-       8,    0,   75,    2, 0x08 /* Private */,
-       9,    0,   76,    2, 0x08 /* Private */,
-      10,    1,   77,    2, 0x08 /* Private */,
-      12,    1,   80,    2, 0x08 /* Private */,
-      13,    0,   83,    2, 0x0a /* Public */,
+       1,    0,   74,    2, 0x08 /* Private */,
+       3,    0,   75,    2, 0x08 /* Private */,
+       4,    0,   76,    2, 0x08 /* Private */,
+       5,    0,   77,    2, 0x08 /* Private */,
+       6,    0,   78,    2, 0x08 /* Private */,
+       7,    0,   79,    2, 0x08 /* Private */,
+       8,    0,   80,    2, 0x08 /* Private */,
+       9,    0,   81,    2, 0x08 /* Private */,
+      10,    0,   82,    2, 0x08 /* Private */,
+      11,    1,   83,    2, 0x08 /* Private */,
+      13,    1,   86,    2, 0x08 /* Private */,
+      14,    0,   89,    2, 0x0a /* Public */,
 
  // slots: parameters
     QMetaType::Void,
@@ -93,8 +95,9 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,   11,
-    QMetaType::Void, QMetaType::Int,   11,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   12,
+    QMetaType::Void, QMetaType::Int,   12,
     QMetaType::Void,
 
        0        // eod
@@ -114,9 +117,10 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 5: _t->colission(); break;
         case 6: _t->cambioEscena(); break;
         case 7: _t->movimientoPersonaje(); break;
-        case 8: _t->on_progressBar_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 9: _t->on_progressBar_Puntuacion_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 10: _t->update(); break;
+        case 8: _t->aumentaPuntaje(); break;
+        case 9: _t->on_progressBar_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 10: _t->on_progressBar_Puntuacion_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->update(); break;
         default: ;
         }
     }
@@ -151,13 +155,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 11)
+        if (_id < 12)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 11;
+        _id -= 12;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 11)
+        if (_id < 12)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 11;
+        _id -= 12;
     }
     return _id;
 }
